e4d751e19b139bb58fee9c9582fa0d45d1ce7857
Anthony Bratcher <tonybrat74@gmail.com>
2020-06-18 15:09:51 -0700

    start stretch

---
+++ src/Quarter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Quarter = () => {
+    return (
+    <div className="quarter">
+    <h3 className="quarter__title">Quarter</h3>
+    <div className="quarter__value">4</div>
+  </div>
+  )
+    }
+export default Quarter;
